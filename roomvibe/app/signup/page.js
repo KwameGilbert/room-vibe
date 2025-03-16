2b3609d40024a1bf030f04d40ec91b04ec7cb0e7
Asemanijoshua <joshuaasemani27@gmail.com>
2025-03-16 01:38:46 +0000

    Modified design to fit modern designs

--- roomvibe/app/signup/page.js
+++ roomvibe/app/signup/page.js
@@ -2,71 +2,94 @@ import Link from "next/link";
 
 export default function signup() {
   return (
-    <section className="pt-5 px-3 sm:hidden">
-      <h1 className="text-2xl text-center pb-3 font-semibold">
-        Create An Account
-      </h1>
+    <main>
+          <section className="pt-10 px-5 sm:hidden">
+            <h1 className="text-2xl text-center pb-10 font-semibold">Create An Account</h1>
+            <form action="" className="flex flex-col gap-3">
+
+              {/* Full Name */}
     
-      <form action="" className="flex flex-col gap-3">
-        {/* this is the username's name */}
-        <div className="flex flex-col gap-2">
-          <label htmlFor="name" className="text-2xl">
-            Username
-          </label>
-          <input
-            type="text"
-            placeholder="John Doe"
-            name="name"
-            required
-            className="h-[55px] border border-slate-950 px-2 text-xl rounded-md focus:outline-none mb-2"/>
-        </div>
-         
-         {/* this the user's email */}
-        <div className="flex flex-col gap-2">
-          <label htmlFor="email" className="text-2xl">
-            Email
-          </label>
-          <input
-            type="email"
-            placeholder="johndoe@gmail.com"
-            name="email"
-            required
-            className="h-[55px] border border-slate-950 px-2 text-xl rounded-md focus:outline-none"
-          />
-        </div>
+              <label htmlFor="fullname" className="text-2xl">
+                Full Name
+              </label>
+              <input
+                type="text"
+                name="fullname"
+                id="fullname"
+                placeholder="Anthony Afriyie"
+                className="h-[60px] border border-slate-950 px-2 text-xl rounded-md mb-5 focus:outline-none"
+                required
+              />
 
-        {/* this is the user's phone number */}
-        <div className="flex flex-col gap-2">
-        <label htmlFor="contact" className="text-2xl">
-            Phone Number
-          </label>
-          <input
-            type="tel"
-            placeholder="johndoe@gmail.com"
-            name="contact"
-            required
-            className="h-[55px] border border-slate-950 px-2 text-xl rounded-md focus:outline-none"
-          />
-        </div>
+              {/* email */}
+    
+              <label htmlFor="email" className="text-2xl">
+                Email
+              </label>
+              <input
+                type="email"
+                name="email"
+                id="email"
+                placeholder="anthonyafriyie@gmail.com"
+                className="h-[60px] border border-slate-950 px-2 text-xl rounded-md mb-5 focus:outline-none"
+                required
+              />
+    
+              {/* password */}
+    
+              <label htmlFor="password" className="text-2xl">
+                Password
+              </label>
+              <input
+                type="password"
+                name="password"
+                id="password"
+                placeholder="Enter your password"
+                className="h-[60px] border border-slate-950 px-2 text-xl rounded-md focus:outline-none mb-5"
+                required
+              />
 
-        {/* this is the user's password */}
-        <div className="flex flex-col gap-2">
-          <label htmlFor="password" className="text-2xl">Password</label>
-          <input
-            type="password"
-            placeholder="enter your password"
-            name="name"
-            required
-            className="h-[55px] border border-slate-950 px-2 text-xl rounded-md focus:outline-none"></input>
-        </div>
+              {/* Phone Number */}
+    
+              <label htmlFor="phoneNumber" className="text-2xl">
+                Phone Number
+              </label>
+              <input
+                type="tel"
+                name="phoneNumber"
+                id="phoneNumber"
+                placeholder="0212345678"
+                pattern="[0-9]{10}"
+                className="h-[60px] border border-slate-950 px-2 text-xl rounded-md focus:outline-none mb-5"
+                required
+              />
 
-        {/* this is the submit button */}
-        <input
-          type="submit"
-          value="Sign Up"
-          className="bg-[#fd7e14] p-3 text-3xl font-bold rounded-md mt-7"
-        />
-      </form>
-    </section>
+              {/* Profile Image */}
+    
+              <label htmlFor="profileImage" className="text-2xl">
+                Profile Image
+              </label>
+              <input
+                type="file"
+                name="profileImage"
+                id="profileImage"
+                className="h-[60px] px-2 text-xl rounded-md focus:outline-none"
+                
+              />
+
+              <p className="text-sm pb-5 text-center">
+                Already have an account?{" "}
+                <Link href="/login" className="underline text-[#fd7e14]">
+                  Login
+                </Link>
+              </p>
+              <input
+                type="submit"
+                value="Sign Up"
+                className="bg-[#fd7e14] p-3 text-3xl font-bold rounded-md mb-10"
+              />
+            </form>
+          </section>
+        </main>
   );
 }
